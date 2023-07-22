fa9d990ed440d42bb31fbe47b43dabc8053809ed
William R <wiribasczky@gmail.com>
2023-07-22 16:31:11 -0300

    Migration que estava faltando para vinculo de tokens e refresh tokens

---
+++ api/database/migrations/2023_07_22_192808_alter_table_token_autenticacao_add_refresh_id.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('token_autenticacao', function (Blueprint $table) {
+            $table->bigInteger('refresh_id')->unsigned()->nullable();
+            $table->foreign('refresh_id')->references('id')->on('refresh_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('token_autenticacao', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('refresh_id');
+        });
+    }
+};
