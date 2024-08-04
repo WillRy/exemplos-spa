45775e9d2eb10f649cbf3b77f5af772adabfb8f5
William R <wiribasczky@gmail.com>
2024-08-04 13:20:59 -0300

    Melhoria de exemplos de eloquent vs query builder

--- api/app/Http/Controllers/Api/TestController.php
+++ api/app/Http/Controllers/Api/TestController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Organizacao;
+use App\Service\HelperArray;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
+
+
     public function testQueries()
     {
         $dados = Organizacao::query()
@@ -18,6 +22,29 @@ class TestController extends Controller
                 $query->where('nome','like','%teste%');
             })
             ->get();
+
+        // $resultado = DB::table('organizacoes')
+        //     ->selectRaw("
+        //         organizacoes.id,
+        //         organizacoes.nome,
+        //         contatos.id as contato_id,
+        //         contatos.nome as contato
+        //     ")
+        //     ->join('contatos', 'organizacoes.id', '=', 'contatos.organizacao_id')
+        //     ->where('contatos.nome', 'like', '%teste%')
+        //     ->get();
+
+        // foreach ($resultado as $item) {
+        //     $dados[$item->id]['id'] = $item->id;
+        //     $dados[$item->id]['organizacao'] = $item->nome;
+        //     $dados[$item->id]['contatos'][] = [
+        //         'id' => $item->contato_id,
+        //         'nome' => $item->contato
+        //     ];
+        // }
+
+        // HelperArray::removerChavesNumericas($dados);
+
 
         return response()->json($dados);
     }
---
+++ api/app/Service/HelperArray.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Service;
+
+
+
+class HelperArray
+{
+    public static function removerChavesNumericas(array &$array)
+    {
+
+        if (empty($array)) {
+            return $array;
+        }
+
+        foreach ($array as $key => &$value) {
+            if (is_array($value)) {
+                self::removerChavesNumericas($value);
+                $array[$key] = $value;
+            }
+        }
+
+        $keys = array_keys($array);
+
+        if (is_numeric($keys[0])) {
+            $array = array_values($array);
+        }
+
+        return $array;
+    }
+}
