023567990b94ff4658532e83c7e3cfe0b2885947
William R <wiribasczky@gmail.com>
2024-09-22 18:19:54 -0300

    Melhoria no axios client - Abstração em classes
    Abstração do uso do axios para permitir trocar de biblioteca
    se for necessário

--- spa-vue/src/services/api.js
+++ spa-vue/src/services/api.js
@@ -2,122 +2,166 @@ import axios from 'axios'
 import { i18n } from '../lang'
 import router from '../router'
 
-let isRefreshing = false
-let failedRequestsQueue = []
 
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-/** requests sem refresh token */
-export const apiPublic = axios.create({
-  baseURL: 'http://localhost:8000/api/',
-  withCredentials: true
-})
+class BaseHttp {
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'http://localhost:8000/api/',
+      withCredentials: true
+    });
 
-/** requests com refresh token, mas SEM redirect automatico para login ao expirar acesso */
-export const apiSemLogout = axios.create({
-  baseURL: 'http://localhost:8000/api/',
-  withCredentials: true
-})
+    axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-/** requests com refresh token, mas COM REDIRECT PARA O LOGIN ao expirar acesso*/
-const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
-  withCredentials: true
-})
+    this.instance.interceptors.request.use(async (config) => {
+      config.headers['Accept-Language'] = i18n.global.locale;
+      config.headers['CSRF-TOKEN'] = ('; ' + document.cookie).split(`; CSRF-TOKEN=`).pop().split(';')[0] ?? null;
+      return config;
+    });
 
-function redirectLogout() {
-  return router.push({
-    name: 'login',
-    query: {
-      logout: 1
-    }
-  })
-}
+    this.isRefreshing = false
+    this.failedRequestsQueue = []
 
-const interceptors = async (config) => {
-  config.headers['Accept-Language'] = i18n.global.locale
-  config.headers['CSRF-TOKEN'] =
-    ('; ' + document.cookie).split(`; CSRF-TOKEN=`).pop().split(';')[0] ?? null
-  return config
-}
-
-const interceptorRefresh = (error, shouldRedirectLogout = true) => {
-  const status = error.response.status
-  // const shouldRefresh = error.response.data?.code === 'token.expired';
-  const shouldRefresh = !error.response.config.url.includes('/refresh')
-
-  if (status === 401) {
-    if (shouldRefresh) {
-      const originalConfig = error.config
-
-      if (!isRefreshing) {
-        isRefreshing = true
-
-        apiPublic
-          .post('/refresh')
-          .then((response) => {
-            const { token } = response.data
-
-            // api.defaults.headers['Authorization'] = `Bearer ${token}`;
-
-            failedRequestsQueue.forEach((request) => request.onSuccess(token))
-            failedRequestsQueue = []
-          })
-          .catch((err) => {
-            failedRequestsQueue.forEach((request) => request.onFailure(err))
-            failedRequestsQueue = []
-
-            if (shouldRedirectLogout) {
-              return redirectLogout(1)
-            }
-          })
-          .finally(() => {
-            isRefreshing = false
-          })
-      }
-
-      return new Promise((resolve, reject) => {
-        failedRequestsQueue.push({
-          // eslint-disable-next-line no-unused-vars
-          onSuccess: (token) => {
-            // originalConfig.headers['Authorization'] = `Bearer ${token}`
-
-            resolve(api(originalConfig))
-          },
-          onFailure: (err) => {
-            reject(err)
-          }
-        })
-      })
-    } else {
-      if (shouldRedirectLogout) {
-        redirectLogout(2)
-      }
-      return Promise.reject(new Error('Sessão expirada'))
-    }
   }
 
-  return Promise.reject(error)
+  redirectLogout() {
+    return router.push({
+      name: 'login',
+      query: {
+        logout: 1
+      }
+    })
+  }
+
+  interceptorRefresh(error, shouldRedirectLogout = true) {
+    const status = error.response.status
+    // const shouldRefresh = error.response.data?.code === 'token.expired';
+    const shouldRefresh = !error.response.config.url.includes('/refresh')
+
+    if (status === 401) {
+      if (shouldRefresh) {
+        const originalConfig = error.config
+
+        if (!this.isRefreshing) {
+          this.isRefreshing = true
+
+          apiPublic
+            .post('/refresh')
+            .then((response) => {
+              const { token } = response.data
+
+              // api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+              this.failedRequestsQueue.forEach((request) => request.onSuccess(token))
+              this.failedRequestsQueue = []
+            })
+            .catch((err) => {
+              this.failedRequestsQueue.forEach((request) => request.onFailure(err))
+              this.failedRequestsQueue = []
+
+              if (shouldRedirectLogout) {
+                return this.redirectLogout(1)
+              }
+            })
+            .finally(() => {
+              this.isRefreshing = false
+            })
+        }
+
+        return new Promise((resolve, reject) => {
+          this.failedRequestsQueue.push({
+            // eslint-disable-next-line no-unused-vars
+            onSuccess: (token) => {
+              // originalConfig.headers['Authorization'] = `Bearer ${token}`
+
+              resolve(this.instance(originalConfig))
+            },
+            onFailure: (err) => {
+              reject(err)
+            }
+          })
+        })
+      } else {
+        if (shouldRedirectLogout) {
+          this.redirectLogout(2)
+        }
+        return Promise.reject(new Error('Sessão expirada'))
+      }
+    }
+
+    return Promise.reject(error)
+  }
+
+  async get(url, config) {
+    return new Promise((resolve, reject) => {
+      this.instance.get(url, config)
+        .then(response => resolve(response))
+        .catch(error => reject(error))
+    });
+  }
+
+  async post(url, data, config) {
+    return new Promise((resolve, reject) => {
+      this.instance.post(url,data, config)
+        .then(response => resolve(response))
+        .catch(error => reject(error))
+    });
+  }
+
+  async put(url, data, config) {
+    return new Promise((resolve, reject) => {
+      this.instance.put(url,data, config)
+        .then(response => resolve(response))
+        .catch(error => reject(error))
+    });
+  }
+
+  async delete(url, config) {
+    return new Promise((resolve, reject) => {
+      this.instance.delete(url, config)
+        .then(response => resolve(response))
+        .catch(error => reject(error))
+    });
+  }
+
+}
+class ApiPublic  extends BaseHttp {
+  constructor() {
+    super();
+  }
 }
 
-//enviar idioma ou token da localstorage(caso use)
-api.interceptors.request.use(interceptors)
+class ApiSemLogout  extends BaseHttp {
+  constructor() {
+    super();
 
-apiPublic.interceptors.request.use(interceptors)
+    this.instance.interceptors.response.use(
+      (response) => {
+        return response
+      },
+      (error) => this.interceptorRefresh(error, false)
+    )
+  }
+}
 
-apiSemLogout.interceptors.request.use(interceptors)
-apiSemLogout.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  (error) => interceptorRefresh(error, false)
-)
+class Api extends BaseHttp {
+  constructor() {
+    super();
 
-api.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  (error) => interceptorRefresh(error, true)
-)
+    this.instance.interceptors.response.use(
+      (response) => {
+        return response
+      },
+      (error) => this.interceptorRefresh(error, true)
+    )
+  }
+}
 
-export default api
+
+const apiPublic = new ApiPublic();
+const apiSemLogout = new ApiSemLogout();
+const api = new Api();
+
+export { apiPublic, apiSemLogout }
+
+export default api;
